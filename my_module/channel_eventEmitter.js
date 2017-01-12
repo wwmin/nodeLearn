7c13a31875c36113da99ba751a2cbe41501a9890
wwmin <wwei.min@163.com>
2017-01-12 11:04:45 +0800

    修正event为events

--- my_module/channel_eventEmitter.js
+++ my_module/channel_eventEmitter.js
@@ -1,4 +1,4 @@
-var EventEmitter = require('event').EventEmitter;
+var EventEmitter = require('events').EventEmitter;
 var channel = new EventEmitter();
 channel.on('join',function(){
   console.log("Welcome!");
