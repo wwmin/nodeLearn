6c9d860ef0c5030a8de55457c8d5bb6539bc9c4a
wwmin <wwei.min@163.com>
2017-01-13 13:10:09 +0800

    http DELETE 操作

--- NodeWeb/CRUD.js
+++ NodeWeb/CRUD.js
@@ -24,6 +24,20 @@ var server = http.createServer(function (req, res) {
       res.setHeader('Content-Type', 'text/plain;charset="utf-8"');
       res.end(body);
       break;
+    case "DELETE":
+      var path = url.parse(req.url).pathname;
+      var i = parseInt(path.slice(1), 10);
+      if (isNaN(i)) {
+        res.statusCode = 400;
+        res.end('Invalid item id');
+      } else if (!items[i]) {
+        res.statusCode = 404;
+        res.end('Item not found');
+      } else {
+        items.splice(i, 1);
+        res.end('OK\n');
+      }
+      break;
   }
 });
 server.listen(3000);
