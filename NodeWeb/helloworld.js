23e876e1c2c704778c86df8c3262fc612ce8dd58
wwmin <wwei.min@163.com>
2017-01-13 10:54:45 +0800

    设定HTTP相应的状态码

--- NodeWeb/helloworld.js
+++ NodeWeb/helloworld.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-  var body = "Hello World!";
+  var url = 'https://www.baidu.com';
+  var body = '<p>Redirecting to <a href="' + url + '">'
+    + url + '</a></p>';
+  res.setHeader('Location', url);
   res.setHeader('Content-length', body.length);
-  res.setHeader('Content-Type','text/plain');
-  res.write(body);
-  res.end();
-  //or
-  // res.end(body);
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 302;
+  res.end(body);
 });
 server.listen(3000);
