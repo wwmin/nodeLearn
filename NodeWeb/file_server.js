f5410f628ee7766cbf5b10430b2457300134b782
wwmin <wwei.min@163.com>
2017-01-13 15:23:51 +0800

    用fs.stat 实现先发致人的错误处理

--- NodeWeb/file_server.js
+++ NodeWeb/file_server.js
@@ -6,18 +6,24 @@ var root = __dirname;
 var server = http.createServer(function (req, res) {
   var url = parse(req.url);
   var path = join(root, url.pathname);
-  var stream = fs.createReadStream(path);
-  /*stream.on('data', function (chunk) {
-   res.write(chunk);
-   });
-   stream.on('end', function () {
-   res.end();
-   });*/
-  //管道
-  stream.pipe(res);
-  stream.on('error',function(err){
-    res.statusCode=500;
-    res.end('Internal Server Error');
+  fs.stat(path, function (err, stat) {
+    if (err) {
+      if ('ENOENT' == err.code) {
+        res.statusCode = 404;
+        res.end('Not Found');
+      } else {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    } else {
+      res.setHeader('Content-Length', stat.size);
+      var stream = fs.createReadStream(path);
+      stream.pipe(res);
+      stream.on('error', function (err) {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      })
+    }
   })
 });
 server.listen(3000);
