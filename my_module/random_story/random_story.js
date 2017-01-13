6a1821c4c9506fe24c5faa5f66c97cb77685e17c
wwmin <wwei.min@163.com>
2017-01-13 09:50:33 +0800

    文章存在性判断

--- my_module/random_story/random_story.js
+++ my_module/random_story/random_story.js
@@ -3,8 +3,8 @@ var request = require('request');
 var htmlparser = require('htmlparser');
 var configFilename = './rss_feeds.txt';
 
-function checkForRSSFile () {
-  fs.exists(configFilename, function(exists) {
+function checkForRSSFile() {
+  fs.exists(configFilename, function (exists) {
     if (!exists)
       return next(new Error('Missing RSS file: ' + configFilename));
 
@@ -12,21 +12,21 @@ function checkForRSSFile () {
   });
 }
 
-function readRSSFile (configFilename) {
-  fs.readFile(configFilename, function(err, feedList) {
+function readRSSFile(configFilename) {
+  fs.readFile(configFilename, function (err, feedList) {
     if (err) return next(err);
 
     feedList = feedList
       .toString()
       .replace(/^\s+|\s+$/g, '')
       .split("\n");
-    var random = Math.floor(Math.random()*feedList.length);
+    var random = Math.floor(Math.random() * feedList.length);
     next(null, feedList[random]);
   });
 }
 
-function downloadRSSFeed (feedUrl) {
-  request({uri: feedUrl}, function(err, res, body) {
+function downloadRSSFeed(feedUrl) {
+  request({uri: feedUrl}, function (err, res, body) {
     if (err) return next(err);
     if (res.statusCode != 200)
       return next(new Error('Abnormal response status code'));
@@ -35,12 +35,12 @@ function downloadRSSFeed (feedUrl) {
   });
 }
 
-function parseRSSFeed (rss) {
+function parseRSSFeed(rss) {
   var handler = new htmlparser.RssHandler();
   var parser = new htmlparser.Parser(handler);
   parser.parseComplete(rss);
 
-  if (!handler.dom.items.length)
+  if (!handler.dom.items||!handler.dom.items.length)
     return next(new Error('No RSS items found'));
 
   var item = handler.dom.items.shift();
@@ -48,10 +48,10 @@ function parseRSSFeed (rss) {
   console.log(item.link);
 }
 
-var tasks = [ checkForRSSFile,
+var tasks = [checkForRSSFile,
   readRSSFile,
   downloadRSSFeed,
-  parseRSSFeed ];
+  parseRSSFeed];
 
 function next(err, result) {
   if (err) throw err;
