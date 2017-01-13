7185527561b0d30e1baa2b7161a0efb7879b1928
wwmin <wwei.min@163.com>
2017-01-13 15:11:29 +0800

    使用管道,将流引入到其他地方

--- NodeWeb/file_server.js
+++ NodeWeb/file_server.js
@@ -7,11 +7,13 @@ var server = http.createServer(function (req, res) {
   var url = parse(req.url);
   var path = join(root, url.pathname);
   var stream = fs.createReadStream(path);
-  stream.on('data', function (chunk) {
-    res.write(chunk);
-  });
-  stream.on('end', function () {
-    res.end();
-  });
+  /*stream.on('data', function (chunk) {
+   res.write(chunk);
+   });
+   stream.on('end', function () {
+   res.end();
+   });*/
+  //管道
+  stream.pipe(res);
 });
 server.listen(3000);
