0df9844b7178cb2b395dbc274841e2b551607f9d
wwmin <wwei.min@163.com>
2017-01-13 12:51:41 +0800

    优化GET,设定Content-Legth头

--- NodeWeb/CRUD.js
+++ NodeWeb/CRUD.js
@@ -17,10 +17,12 @@ var server = http.createServer(function (req, res) {
       });
       break;
     case "GET":
-      items.forEach(function (item, i) {
-        res.write(i + ') ' + item + '\n');
-      });
-      res.end();
+      var body = items.map(function (item, i) {
+        return i + ') ' + item;
+      }).join('\n');
+      res.setHeader('Content-Length', Buffer.byteLength(body));
+      res.setHeader('Content-Type', 'text/plain;charset="utf-8"');
+      res.end(body);
       break;
   }
 });
