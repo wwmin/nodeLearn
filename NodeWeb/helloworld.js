761e4c25b7f0974f4b0358fab27dbf03032cdcca
wwmin <wwei.min@163.com>
2017-01-13 10:44:36 +0800

    设定相应头

--- NodeWeb/helloworld.js
+++ NodeWeb/helloworld.js
@@ -1,6 +1,11 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-  res.write("Hello World.");
+  var body = "Hello World!";
+  res.setHeader('Content-length', body.length);
+  res.setHeader('Content-Type','text/plain');
+  res.write(body);
   res.end();
+  //or
+  // res.end(body);
 });
 server.listen(3000);
