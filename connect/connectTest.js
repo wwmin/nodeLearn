a0f2ee10f8c0ea442a8e4fa528221ae0d30f44fa
wwmin <wwei.min@163.com>
2017-01-15 14:58:38 +0800

    使用多个connect中间件组件

--- connect/connectTest.js
+++ connect/connectTest.js
@@ -1,9 +1,14 @@
 var connect = require('connect');
-var app = connect();
-app.use(logger);
-app.listen(3000);
 
 function logger(req, res, next) {
   console.log('%s %s', req.method, req.url);
   next();
 }
+function hello(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hello world.');
+}
+connect()
+  .use(logger)
+  .use(hello)
+  .listen(3000);
