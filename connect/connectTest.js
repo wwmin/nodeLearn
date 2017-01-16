e3b140d25ea84c9a85f205ead10ff4d2992bed7d
wwmin <wwei.min@163.com>
2017-01-16 10:27:43 +0800

    完善用baseAuth验证

--- connect/connectTest.js
+++ connect/connectTest.js
@@ -8,17 +8,40 @@ function hello(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.end('hello world.');
 }
+function authenticateWidthDatabase(user, pass, callback) {
+  var err;
+  if (user != 'tobi' || pass != 'ferret') {
+    err = new Error('Unauthorized');
+  }
+  callback(err);
+}
 function restrict(req, res, next) {
-  console.log(`注册过滤:${req.method} ${req.url}`);
-  next();
+  var authorization = req.headers.authorization;
+  if (!authorization)return next(new Error('Unauthorized'));
+  var parts = authorization.split(' ');
+  var scheme = parts[0];
+  var auth = new Buffer(parts[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  authenticateWidthDatabase(user, pass, function (err) {
+    if (err)return next(err);
+    next();
+  });
 }
 function admin(req, res, next) {
-  console.log(`admin:${req.method} ${req.url}`);
-  next();
+  switch (req.url) {
+    case '/':
+      res.end('try /users');
+      break;
+    case '/users':
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(['tobi', 'loki', 'jane']));
+      break;
+  }
 }
 connect()
   .use(logger)
-  .use('./admin',restrict)
-  .use('./admin',admin)
+  .use('/admin', restrict)
+  .use('/admin', admin)
   .use(hello)
   .listen(3000);
