1be97272c126b6f1cfa2aaf8b9983b8079fa1dcc
wwmin <wwei.min@163.com>
2017-01-16 09:54:25 +0800

    添加访问路径中间件前缀

--- connect/connectTest.js
+++ connect/connectTest.js
@@ -8,7 +8,17 @@ function hello(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.end('hello world.');
 }
+function restrict(req, res, next) {
+  console.log(`注册过滤:${req.method} ${req.url}`);
+  next();
+}
+function admin(req, res, next) {
+  console.log(`admin:${req.method} ${req.url}`);
+  next();
+}
 connect()
   .use(logger)
+  .use('./admin',restrict)
+  .use('./admin',admin)
   .use(hello)
   .listen(3000);
