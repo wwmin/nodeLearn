6e0172d7359f4611899459438e9d509c2bfcb6c1
wwmin <wwei.min@163.com>
2017-01-16 22:15:17 +0800

    设置cookie

--- connect_self/cookie.js
+++ connect_self/cookie.js
@@ -3,6 +3,9 @@ var cookieParser=require('cookie-parser');
 var app = connect()
   .use(cookieParser('wwmin'))
   .use(function (req, res) {
+    res.setHeader('Set-Cookie','foo=bar');
+    res.setHeader('Set-Cookie','tobi=ferret;' +
+      'Expires=Tue,08 Jun 2021 10:10:10 GMT');
     console.log(req.cookies);
     console.log(req.signedCookies);
     res.end('hello\n');
