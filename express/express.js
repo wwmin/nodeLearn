0b63717acc72aeb5254507b402db03ed246cf11d
wwmin <wwei.min@163.com>
2017-01-19 11:25:38 +0800

    测试驱动开发 express REST API

--- express/express.js
+++ express/express.js
@@ -21,5 +21,32 @@ app.post('/collections/:collectionName', function (req, res, next) {
   })
 });
 
+app.get('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.findById(req.params.id, function (e, result) {
+    if (e)return next(e);
+    res.send(result);
+  })
+});
+
+app.get('/collections/:collectionName', function (req, res, next) {
+  req.collection.find({}, {limit: 10, sort: [['_id', -1]]}).toArray(function (e, results) {
+    if (e)return next(e);
+    res.send(results)
+  })
+});
+
+app.put('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.updateById(req.params.id, {$set: req.body}, function (e, result) {
+    if (e)return next(e);
+    res.send(result);
+  })
+});
+
+app.del('/collections/:collectionName/:id',function (req, res, next) {
+  req.collection.removeById(req.params.id,function (e, result) {
+    if(e)return next(e);
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+});
 //----------------
 app.listen(3000);
--- express/express.test.js
+++ express/express.test.js
@@ -8,7 +8,7 @@ describe('express rest api server', function () {
   before(function () {
     db.collection('test').remove({})
   });
-  var id
+  var id;
   //--测试post
   it('post object', function (done) {
     superagent.post('http://localhost:3000/collections/test')
@@ -17,66 +17,66 @@ describe('express rest api server', function () {
         email: 'John@rpjs.co'
       })
       .end(function (e, res) {
-        expect(e).to.eql(null)
-        expect(res.body.ops.length).to.eql(l)
-        expect(res.body.ops[0]._id.length).to.eql(24)
-        id = res.body.ops[0]._id
+        expect(e).to.eql(null);
+        expect(res.body.ops.length).to.eql(1);
+        expect(res.body.ops[0]._id.length).to.eql(24);
+        id = res.body.ops[0]._id;
         done()
       })
-  })
+  });
 
   //--测试get by ID
   it('retrieves an object', function (done) {
     superagent.get('http://localhost:3000/collections/test/' + id)
       .end(function (e, res) {
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body._id.length).to.eql(24)
-        expect(res.body._id).to.eql(id)
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
         done()
       })
-  })
+  });
   //--测试get 集合
   it('retrieves a collection', function (done) {
     superagent.get('http://localhost:3000/collections/test')
       .end(function (e, res) {
-        expect(e).to.eql(null)
-        expect(res.body.length).to.be.above(0)
+        expect(e).to.eql(null);
+        expect(res.body.length).to.be.above(0);
         expect(res.body.map(function (item) {
           return item._id
-        })).to.contain(id)
+        })).to.contain(id);
         done()
       })
-  })
+  });
 
   //--测试update 更新
-  it('updates an object',function (done) {
-    superagent.put('http://localhost:3000/collections/test/'+id)
+  it('updates an object', function (done) {
+    superagent.put('http://localhost:3000/collections/test/' + id)
       .send({
-        name:'Peter',
-        email:'peter@yahoo.com'
+        name: 'Peter',
+        email: 'peter@yahoo.com'
       })
       .end(function (e, res) {
-        expect(e).to.eql(null)
+        expect(e).to.eql(null);
         done()
       })
-  })
+  });
   //--验证更新后的数据
-  it('checks an updated object',function (done) {
-    superagent.get('http://localhost:3000/collections/test/'+id)
+  it('checks an updated object', function (done) {
+    superagent.get('http://localhost:3000/collections/test/' + id)
       .end(function (e, res) {
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body._id.length).to.eql(24)
-        expect(res.body._id).to.eql(id)
-        expect(res.body.name).to.eql('Peter')
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
+        expect(res.body.name).to.eql('Peter');
         done()
       })
-  })
+  });
 
   //--测试remove删除
-  it('removes an object',function (done) {
-    superagent.del('http://localhost:3000/collections/test/'+id)
+  it('removes an object', function (done) {
+    superagent.del('http://localhost:3000/collections/test/' + id)
       .end(function (e, res) {
         expect(e).to.eql(null)
         expect(res.body.msg).to.eql('success')
